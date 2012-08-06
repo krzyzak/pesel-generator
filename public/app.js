8d71f1271b24a67a4624471a9a58b289896840fe
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2012-08-06 19:16:05 +0200

    client side date validation

--- public/app.js
+++ public/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+  $("input[type=submit]").attr("disabled", "disabled");
   $("#date").datepicker({
       changeMonth: true,
       changeYear: true,
       dateFormat: "yy-mm-dd"
     });
+  $("#date").change(function(){
+    if(Date.parse($(this).val())){
+      $("input[type=submit]").removeAttr("disabled");
+    }
+    else{
+      $("input[type=submit]").attr("disabled", "disabled");
+    }
+  });
 });
